f84e74fe9c916cc2cfa60d372588fb1c4c235d5b
Manuel Moran <manuel.moran@arrivia.com>
2024-09-09 13:50:27 -0700

    Framework facade initial version, IConfigurationService registered.

---
+++ ATM-NET-Advanced/FrameworkFacade/FrameworkStartup/FrameworkService.cs
@@ -0,0 +1,26 @@
+﻿using ConfigurationLibrary.Configurations;
+using ConfigurationLibrary.Interfaces.Configurations;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace FrameworkFacade.FrameworkStartup;
+
+public class FrameworkService
+{
+	private readonly IServiceProvider _serviceProvider;
+
+	public FrameworkService(string filePath, string fileName)
+	{
+		ArgumentException.ThrowIfNullOrWhiteSpace(filePath);
+		ArgumentException.ThrowIfNullOrWhiteSpace(fileName);
+
+		_serviceProvider = new ServiceCollection()
+			.AddSingleton<IConfigurationService>(new ConfigurationService(filePath, fileName))
+			.BuildServiceProvider();
+	}
+
+	public IServiceProvider GetServiceProvider()
+	{
+		return _serviceProvider;
+	}
+}
