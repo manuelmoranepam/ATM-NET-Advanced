1088fa571d13c3e1107054ce39dc75e11178b3bb
Manuel Moran <manuel.moran@arrivia.com>
2024-09-12 14:14:44 -0700

    WebDriverLibrary project created, IWebDriverConfiguration and IWebDriverService implemented and registered in the Framework.

--- ATM-NET-Advanced/FrameworkFacade/FrameworkStartup/FrameworkService.cs
+++ ATM-NET-Advanced/FrameworkFacade/FrameworkStartup/FrameworkService.cs
@@ -4,6 +4,10 @@ using LoggerLibrary.Interfaces.Loggers;
 using LoggerLibrary.Loggers;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using WebDriverLibrary.Configurations;
+using WebDriverLibrary.Interfaces.Configurations;
+using WebDriverLibrary.Interfaces.WebDrivers;
+using WebDriverLibrary.WebDrivers;
 
 namespace FrameworkFacade.FrameworkStartup;
 
@@ -19,6 +23,8 @@ public class FrameworkService
 		_serviceProvider = new ServiceCollection()
 			.AddSingleton<IConfigurationService>(new ConfigurationService(filePath, fileName))
 			.AddScoped<ILoggerService, SerilogLoggerService>()
+			.AddSingleton<IWebDriverConfiguration, WebDriverConfiguration>()
+			.AddScoped<IWebDriverService, SeleniumWebDriverService>()
 			.BuildServiceProvider();
 	}
 
---
+++ ATM-NET-Advanced/WebDriverLibrary/Interfaces/WebDrivers/IWebDriverService.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+using WebDriverLibrary.Interfaces.Configurations;
+
+namespace WebDriverLibrary.Interfaces.WebDrivers;
+
+public interface IWebDriverService
+{
+	void DisposeWebDriver();
+	string GetCurrentPageTitle();
+	string GetCurrentUrl();
+	IWebDriver GetWebDriver();
+	IWebDriverConfiguration GetWebDriverConfiguration();
+	void NavigateTo(string url);
+}
